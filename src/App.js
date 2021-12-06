d429528b520936f3ccfc5aad6be876a39f25992d
Miguel Garcia <gunsandgamer@hotmail.com>
2021-12-06 16:44:02 -0600

    tailwind

--- src/App.js
+++ src/App.js
@@ -4,48 +4,46 @@
  * COMPONENTES
  * 1. IMPORTACIONES
  */
-import './App.css';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Body from './components/Body'
-import Main from './components/Main'
-
-/**
- * 
- * 2. FUNCIÓN
- */
-
-function App() {
-
-  // MANEJO DE VARIABLES Y OTRAS FUNCIONES
-  const bootcamp = "Ironhack"
-
-  // ESTE FORMATO NO ES HTML COMPLETAMENTE, SINO TAMBIÉN INVOLUCRA JAVASCRIPT AL SIMULTÁNEO.
-  // HTML + JS = JSX
-  return ( 
-    <div className="App">
-      <p>Hola mundo</p>
-      <p>Mike Nieva</p>
-      <p>{12*12}</p>
-      <p>{bootcamp}</p>
-
-
-      <Header nombre="Mike" ciudad="Veracruz"/>
-      <Header nombre="Sam" ciudad="Edomex"/>
-      <Header nombre="K'onhin" ciudad="Hidalgo" />
-      <Body cancion="ataud" album="realidades" grupo="Los Tigres del Norte"/>
-
-      <hr />
-      <Main />
-      <hr />
-
-      <Footer nombre="Anahi" email="hola@hola.com"/>
-
-
-    </div>
-  );
-}
-
-
-// 3. EXPORTACIÓN
-export default App;
+ import './App.css';
+ import Header from './components/Header'
+ import Footer from './components/Footer'
+ import Main from './components/Main'
+ 
+ /**
+  * 
+  * 2. FUNCIÓN
+  */
+ 
+ function App() {
+ 
+   // MANEJO DE VARIABLES Y OTRAS FUNCIONES
+   const bootcamp = "Ironhack"
+ 
+   // ESTE FORMATO NO ES HTML COMPLETAMENTE, SINO TAMBIÉN INVOLUCRA JAVASCRIPT AL SIMULTÁNEO.
+   // HTML + JS = JSX
+   return ( 
+     <div className="App">
+       <Header nombre="Mike" ciudad="Veracruz" />
+       <p>Hola mundo</p>
+       <p>Mike Nieva</p>
+       <p>{12*12}</p>
+       <p>{bootcamp}</p>
+             
+       {/* <Header nombre="Sam" ciudad="EdoMex" />
+       <Header nombre="K'onhin" ciudad="Hidalgo" /> */}
+ 
+       <hr />
+ 
+       <Main />
+ 
+       <hr />
+       <Footer nombre="Anahí" email="hola@hola.com" />
+ 
+ 
+     </div>
+   );
+ }
+ 
+ 
+ // 3. EXPORTACIÓN
+ export default App;
